36b10f329164a2f4653000056b3257361eaf7ce3
druidim <gingerwolf21@gmail.com>
2024-09-11 19:41:45 -0400

    made white rectangle on red background

--- weekly topics/instructions/function-calls/js/script.js
+++ weekly topics/instructions/function-calls/js/script.js
@@ -10,7 +10,7 @@
 
 function setup() {
     // Once at the beginning of the program
-    createCanvas{ 640, 480 };
+    createCanvas(640, 480);
 }
 
 
@@ -20,4 +20,5 @@ function setup() {
 function draw() {
     // Every frame
     background(255, 100, 100);
+    rect(200, 80, 240, 320)
 }
