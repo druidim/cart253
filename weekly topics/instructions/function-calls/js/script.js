e01d124a7d5ae2d9a6f6e59c4bead6d44b6ce49c
druidim <gingerwolf21@gmail.com>
2024-09-11 20:00:26 -0400

    Made comments detailing each function and the use of the program.

--- weekly topics/instructions/function-calls/js/script.js
+++ weekly topics/instructions/function-calls/js/script.js
@@ -1,24 +1,34 @@
 /**
- * Function Calls
+ * The Blank Page
+ * (formerly "Function Calls")
  * Laura Slabbert
  * 
- * Working on the function calls examples
+ * An exploration of the existentail angst of a novelist
+ * who must sit down at their pink (red) desk and confront
+ * the abyss that is a blank page of paper.
+ * 
+ * The program is non-interactive to convey the inability to get started on the project.
+ * (Formerly "Working on the function calls examples")
  */
 
 "use strict";
 
-
+/**
+ * Creates a canvas for our masterpiece.
+ */
 function setup() {
-    // Once at the beginning of the program
+    // Create a canvas at a standard resolution. (Formerly "Once at the beginning of the program")
     createCanvas(640, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the writer's desktop and a blank piece of paper.
 */
 function draw() {
     // Every frame
+    // The RED desktop.
     background(255, 100, 100);
+    // The piece of paper.
     rect(200, 80, 240, 320)
 }
