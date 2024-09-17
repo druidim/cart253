6d85c2815b60bb02849901b5d1e4b59fef7f11fb
druidim <gingerwolf21@gmail.com>
2024-09-16 21:57:33 -0400

    made the italian flag!! but a bit too over to the right

--- weekly topics/instructions/challenges/italian flag/README.md
+++ weekly topics/instructions/challenges/italian flag/README.md
@@ -1,4 +1,4 @@
-# Template p5 Project
+# Italian Flag
 
 Author Name
 
--- weekly topics/instructions/challenges/italian flag/js/script.js
+++ weekly topics/instructions/challenges/italian flag/js/script.js
@@ -1,24 +1,47 @@
 /**
- * Title of Project
- * Author Name
+ * Italian Flag
+ * Laura Slabbert
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * I am going to try to draw an Italian flag within this code.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!(i dont really know what to put here...
+ * open program? look at screen?)
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
-
-
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * same issue as function setup :-/
 */
-function draw() {
 
+function draw() {
+    //draws the background
+    push();
+    background(100, 149, 237);
+    pop();
+
+    //draws the leftmost rectangle
+    push();
+    fill(0, 140, 69);
+    noStroke();
+    rect(100, 160, 160, 320);
+    pop();
+
+    //draws the middle rectangle
+    push();
+    fill(244, 249, 255);
+    noStroke();
+    rect(260, 160, 160, 320);
+    pop();
+
+    //draws the leftmost rectangle
+    push();
+    fill(205, 33, 42);
+    noStroke();
+    rect(420, 160, 160, 320);
+    pop();
 }
