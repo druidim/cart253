1f82f6b27592efa414900c8337c919d5e03cf4d1
druidim <gingerwolf21@gmail.com>
2024-09-18 23:36:25 -0400

    finished variables in practice movement (bird) project

--- weekly topics/variables/Variables in Practice Movement/js/script.js
+++ weekly topics/variables/Variables in Practice Movement/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Variables in Practice Movement
+ * Laura Slabbert
  * 
  * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
  * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
@@ -8,17 +8,54 @@
 
 "use strict";
 
+let bird = {
+    x: 120,
+    y: 480,
+    size: 50,
+    velocity: {
+        x: 0,
+        y: 0,
+    },
+    minVelocity: {
+        x: -3,
+        y: -2,
+    },
+    maxVelocity: {
+        x: 3,
+        y: 2,
+    },
+    acceleration: {
+        x: 0.025,
+        y: -0.05
+    }
+}
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(640, 480);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * move the bird and display it
 */
 function draw() {
+    background(0);
 
+    //move the bird
+    bird.velocity.x = bird.velocity.x + bird.acceleration.x;
+    bird.velocity.y = bird.velocity.y + bird.acceleration.y;
+
+    bird.velocity.x = constrain(bird.velocity.x, bird.minVelocity.x, bird.maxVelocity.x);
+    bird.velocity.y = constrain(bird.velocity.y, bird.minVelocity.y, bird.maxVelocity.y);
+
+    bird.x = bird.x + bird.velocity.x;
+    bird.y = bird.y + bird.velocity.y;
+    //draw the bird
+    push();
+    fill(255, 0, 0)
+    ellipse(bird.x, bird.y, bird.size);
+    pop();
 }
