f270d3d67bbbd6b85e41499899a521866c8e4f9d
druidim <gingerwolf21@gmail.com>
2024-09-18 20:34:22 -0400

    finished the creating variables work - made a more legible cheese program

--- weekly topics/variables/Creating variables/js/script.js
+++ weekly topics/variables/Creating variables/js/script.js
@@ -7,6 +7,15 @@
 
 "use strict";
 
+
+let cheeseRed = 255;
+let cheeseGreen = 255;
+let cheeseBlue = 0;
+
+let holeShade = 0;
+let holeSize = 120;
+let holeX = 140;
+let holeY = 175;
 /**
  *Creates the canvas
 */
@@ -20,12 +29,12 @@ function setup() {
 */
 function draw() {
     //the cheese
-    background(255, 255, 0)
+    background(cheeseRed, cheeseGreen, cheeseBlue);
 
     //the hole
     push();
     noStroke();
-    fill(0);
-    ellipse(140, 175, 180);
+    fill(holeShade);
+    ellipse(140, 175, holeSize);
     pop();
 }
