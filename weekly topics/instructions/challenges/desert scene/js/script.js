7b8f52bc6d55289683fc03891a08753c23315554
druidim <gingerwolf21@gmail.com>
2024-09-21 14:48:38 -0400

    finished the pyramid !

--- weekly topics/instructions/challenges/desert scene/js/script.js
+++ weekly topics/instructions/challenges/desert scene/js/script.js
@@ -1,24 +1,50 @@
 /**
- * Title of Project
- * Author Name
+ * Desert Scene
+ * Laura Slabbert
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This program draws a picture of a pyramid on some dunes.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the pyramid and the sand dunes
 */
 function draw() {
+    //draws the blue sky
+    background(137, 207, 240);
 
+    //draws the lower sand dune
+    push();
+    noStroke();
+    fill(255, 234, 0);
+    ellipse(360, 800, 1000, 900);
+
+    //draws the higher sand dune
+    noStroke();
+    fill(255, 234, 0);
+    ellipse(0, 700, 1000, 900);
+    pop();
+
+    //draws the shaded side of the pyramid
+    push();
+    noStroke();
+    fill(139, 128, 0);
+    triangle(330, 420, 320, 280, 410, 390)
+    pop();
+
+    //draws the lit up side of the pyramid
+    push();
+    noStroke();
+    fill(228, 208, 10);
+    triangle(250, 400, 320, 280, 330, 420)
+    pop();
 }
