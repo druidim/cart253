dfd9ae6075fd21273835bee39608554896e5fba9
druidim <gingerwolf21@gmail.com>
2024-09-21 16:30:55 -0400

    started and finished the smiley face (after a pizza break)

--- weekly topics/instructions/challenges/smiley face/js/script.js
+++ weekly topics/instructions/challenges/smiley face/js/script.js
@@ -1,24 +1,53 @@
 /**
- * Title of Project
- * Author Name
+ * Smiley Face
+ * laura Slabbert
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * This program draws a smiley face
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws the smiley face
 */
 function draw() {
+    //draws the background
+    push();
+    background(231, 190, 243);
+    pop();
+    //draws the face (skin...?)
+    push();
+    noStroke();
+    fill(255, 238, 139)
+    ellipse(320, 320, 320, 320);
+    pop();
 
+    //draws the left eye
+    push();
+    noStroke();
+    fill(0)
+    ellipse(255, 270, 40, 60);
+    pop();
+
+    //draws the right eye
+    push();
+    noStroke();
+    fill(0)
+    ellipse(385, 270, 40, 60);
+    pop();
+
+    //draws the smile
+    push();
+    strokeWeight(10);
+    noFill();
+    arc(320, 300, 200, 250, 0, PI);
+    pop();
 }
