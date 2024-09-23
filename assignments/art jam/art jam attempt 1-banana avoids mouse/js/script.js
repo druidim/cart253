fd84563560ce13747df5251aa208d93a9a240a90
druidim <gingerwolf21@gmail.com>
2024-09-23 19:38:57 -0400

    started attempt #! of banana project

---
+++ assignments/art jam/art jam attempt 1-banana avoids mouse/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Banana avoids mouse
+ * Laura Slabbert
+ * 
+ * A banana does not wish to be eaten and avoids the mouse
+ */
+
+"use strict";
+
+/**
+ * Draws the canvas
+*/
+function setup() {
+    createCanvas(640, 640)
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(204, 204, 255);
+}
