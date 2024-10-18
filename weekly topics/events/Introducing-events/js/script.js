f01d793ff4f72749bb3cd45d1386e4600d18591f
druidim <gingerwolf21@gmail.com>
2024-10-18 19:46:47 -0400

    finished "introducing events" module

--- weekly topics/events/Introducing-events/js/script.js
+++ weekly topics/events/Introducing-events/js/script.js
@@ -10,11 +10,20 @@
 
 
 function setup() {
-
+    createCanvas(400, 400);
+    background(0);
 }
 
 
 
 function draw() {
 
+}
+
+function mousePressed() {
+    push();
+    noStroke();
+    fill(255, 255, 0);
+    ellipse(mouseX, mouseY, 50);
+    pop();
 }
