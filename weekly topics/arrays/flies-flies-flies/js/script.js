7ed1e17ca024deb1f363c1f8f5e35664466ea316
druidim <gingerwolf21@gmail.com>
2024-11-09 15:31:58 -0500

    finished fliesfliesflies

--- weekly topics/arrays/flies-flies-flies/js/script.js
+++ weekly topics/arrays/flies-flies-flies/js/script.js
@@ -9,32 +9,24 @@
 
 // Our array of flies (specifically "fly data" really)
 // Each fly has a position and a size !
-let flies = [
-    {
-        x: 100,
-        y: 125,
-        size: 10,
-        buzziness: 2
-    },
-    {
-        x: 160,
-        y: 170,
-        size: 14,
-        buzziness: 4
-    },
-    {
-        x: 180,
-        y: 50,
-        size: 5,
-        buzziness: 6
-    }
-];
+let flies = [];
 
 /**
  * Creates the canvas
  */
 function setup() {
     createCanvas(300, 300);
+}
+
+
+function createFly() {
+    let fly = {
+        x: random(0, width),
+        y: random(0, height),
+        size: random(2, 10),
+        buzziness: random(2, 8),
+    };
+    return fly;
 }
 
 /**
@@ -68,4 +60,24 @@ function drawFly(fly) {
     fill(0);
     ellipse(fly.x, fly.y, fly.size);
     pop();
+}
+
+function keyPressed() {
+    //add a new fly to the array
+    const newFly = createFly();
+    flies.push(newFly);
+}
+
+function mousePressed() {
+    //check all the flies..
+    for (let fly of flies) {
+        const d = dist(mouseX, mouseY, fly.x, fly.y);
+        if (d < fly.size / 2) {
+            //this fly was clicked!
+            //get the index of this fly
+            const index = flies.indexOf(fly);
+            //splice it out of the array
+            flies.splice(index, 1);
+        }
+    }
 }
