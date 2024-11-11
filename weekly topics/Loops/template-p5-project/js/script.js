3c4e95e163c565c1d0c28aad87c0f6c70840f6b1
druidim <gingerwolf21@gmail.com>
2024-11-10 20:34:54 -0500

    finished for loops in practice

--- weekly topics/Loops/template-p5-project/js/script.js
+++ weekly topics/Loops/template-p5-project/js/script.js
@@ -8,10 +8,14 @@
 
 "use strict";
 
+const numStars = 100;
+
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
+    createCanvas(400, 400);
 
 }
 
@@ -21,4 +25,23 @@ function setup() {
 */
 function draw() {
 
+    background(0);
+
+    randomSeed(1);
+
+    for (let i = 0; i < numStars; i++) {
+        drawStar();
+    }
+}
+
+function drawStar() {
+    const x = random(0, width);
+    const y = random(0, height);
+    const diameter = random(2, 5);
+
+    push();
+    fill(255);
+    noStroke();
+    ellipse(x, y, diameter);
+    pop();
 }
--- weekly topics/Loops/template-p5-projectTWO/js/script.js
+++ weekly topics/Loops/template-p5-projectTWO/js/script.js
@@ -12,7 +12,7 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(600, 300);
 }
 
 
@@ -20,5 +20,15 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+
+    background(0);
+
+    for (let x = 0; x <= width; x++) {
+        const shade = map(x, 0, width, 0, 255);
+        push();
+        stroke(shade);
+        line(x, 0, x, height);
+        pop();
+    }
 
 }
